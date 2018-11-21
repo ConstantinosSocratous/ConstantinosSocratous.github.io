d2e5b9e64da7ee2cf20fb5086c4747cd40bf6917
ConstantinosSocratous <constantinos.socratous@cytanet.com.cy>
2018-11-21 22:27:19 +0000

    Initial commit

---
+++ pages/skills.php
@@ -0,0 +1,17 @@
+<section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+  <div class="my-auto">
+    <h2 class="mb-5">Skills</h2>
+
+    <div class="subheading mb-3">Programming Languages &amp; Computer Skills</div>
+
+    <ul class="fa-ul mb-0">
+        <li>Java</li>
+        <li>C++</li>
+        <li>HTML</li>
+        <li>php</li>
+        <li>MySQL</li>
+        <li>Agile Development &amp; Scrum</li>
+        <li>Microsoft Word, Excel, PowerPoint, Access</li>
+    </ul>
+  </div>
+</section>
